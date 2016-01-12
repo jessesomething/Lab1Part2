82164c1390ecd51dbfe77bc43ffec97e388febe5
jessesomething <jessespalding@gmail.com>
2016-01-12 16:51:15 -0600

    Getting close somehow..

--- Part 2.py
+++ Part 2.py
@@ -4,28 +4,50 @@ sentence = sentence.split(" ")
 
 parsedSentence = ''
 
-wordCount = 0
+# Started working on the methods instead after struggling with the code below
 
-while wordCount < len(sentence):
-    firstUpper = ''
-    restLetters = ''
-    if wordCount == 0:
-        firstWord = sentence[wordCount].lower()
-        parsedSentence = firstWord
-    else:
-        letterCount = 0
-        while letterCount < len(sentence[wordCount]):
-        # for letter in sentence[wordCount]:
-            if letterCount == 0:
-                sentence[wordCount[0]].upper()
-            else:
-                sentence[wordCount[1:]].lower()
-            letterCount = letterCount + 1
+# Uppercase parser
+def parseUppers(word):
+    letterStr = word
+    letterStr = letterStr.upper()
+    return letterStr
 
-    wordCount = wordCount + 1
-    parsedSentence = parsedSentence
+# Lowercase parser
+def parseLowers(word):
+    letterStr = word
+    letterStr = letterStr.lower()
+    return letterStr
 
-print(parsedSentence)
+firstWord = parseLowers(sentence[0])
+
+# Need to use this method with a loop instead of trying to apply to a range
+# lastWords = parseLowers(sentence[0:])
+
+print(firstWord)
+# print(lastWords)
+
+# wordCount = 0
+#
+# while wordCount < len(sentence):
+#     firstUpper = ''
+#     restLetters = ''
+#     if wordCount == 0:
+#         firstWord = sentence[wordCount].lower()
+#         parsedSentence = firstWord
+#     else:
+#         letterCount = 0
+#         while letterCount < len(sentence[wordCount]):
+#         # for letter in sentence[wordCount]:
+#             if letterCount == 0:
+#                 sentence[wordCount[0]].upper()
+#             else:
+#                 sentence[wordCount[1:]].lower()
+#             letterCount = letterCount + 1
+#
+#     wordCount = wordCount + 1
+#     parsedSentence = parsedSentence
+#
+# print(parsedSentence)
 
 # for letter in sentence:
 #     sentence[letter] =
@@ -35,16 +57,5 @@ print(parsedSentence)
 #     if letter == ' ':
 #         letter.next.upper()
 
-def parseUppers(letter):
-    letterStr = letter
-    for letter in letterStr:
-        letter.upper()
-    return letterStr
 
-def parseLowers(letter):
-    letterStr = letter
-    for letter in letterStr:
-        letterStr.lower()
-    return letterStr
-
-print(sentence)
+# print(sentence)
